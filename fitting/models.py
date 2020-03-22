d057949bc7b36dee7dc7929f6ce11a49806c8146
yzi2 <yzi2@central.uh.edu>
2020-03-22 13:26:14 -0500

    update

--- fitting/models.py
+++ fitting/models.py
@@ -1,9 +1,8 @@
 import numpy as np
-# %%
 import cv2
-image1 = cv2.imread('/home/cougarnet.uh.edu/yzi2/PycharmProjects/cv-homework-2-YuanZi1501040205/2-in000001.jpg')
-image2 = cv2.imread('/home/cougarnet.uh.edu/yzi2/PycharmProjects/cv-homework-2-YuanZi1501040205/2-in001800.jpg')
-# %%
+# image1 = cv2.imread('/home/cougarnet.uh.edu/yzi2/PycharmProjects/cv-homework-2-YuanZi1501040205/2-in000001.jpg')
+# image2 = cv2.imread('/home/cougarnet.uh.edu/yzi2/PycharmProjects/cv-homework-2-YuanZi1501040205/2-in001800.jpg')
+
 class FittingModels:
 
     def create_dataPoints(self, image1, image2):
@@ -15,15 +14,31 @@ class FittingModels:
         image1 = cv2.cvtColor(image1, cv2.COLOR_BGR2GRAY)
         image2 = cv2.cvtColor(image2, cv2.COLOR_BGR2GRAY)
         points = []
-        for i in range()
+        for i in range(image1.shape[0]):
+            for j in range(image1.shape[1]):
+                points.append([image1[i][j], image2[i][j]])
+        return points
 
     def plot_data(self, data_points):
         """ Plots the data points
         :param data_points:
         :return: an image
         """
-        test_image_return = np.zeros((100, 100), np.uint8) #test remove before submission
-        return test_image_return
+        import io
+        from PIL import Image
+        import matplotlib.pyplot as plt
+        #convert points' pair to x, y for plotting
+        x, y = np.array(data_points).T
+        #create figure and read it as image object
+        plt.figure()
+        plt.plot(x, y, 'bo')
+        buf = io.BytesIO()
+        plt.savefig(buf, format='png')
+        buf.seek(0)
+        im = Image.open(buf)
+        im.show()
+        buf.close()
+        return im
 
     def fit_line_ls(self, data_points, threshold):
         """ Fits a line to the given data points using least squares
